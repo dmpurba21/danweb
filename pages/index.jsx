ba6a9aa0e77a0c5610c06f74c3c3c8bfabc847e8
dmpurba21 <dmpurba@icloud.com>
2026-05-22 15:52:24 +0700

    index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,7 @@
 import { motion } from "framer-motion";
-
 import ParticlesContainer from "../components/ParticlesContainer";
 import ProjectsBtn from "../components/ProjectsBtn";
 import Avatar from "../components/Avatar";
-
 import { fadeIn } from "../variants";
 
 const Home = () => {
@@ -13,30 +11,28 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-         {/* title */}
-<motion.h1
-  variants={fadeIn("down", 0.2)}
-  initial="hidden"
-  animate="show"
-  exit="hidden"
-  className="h1"
->
-  Turning Data <br /> Into{" "}
-  <span className="text-accent">Business Value.</span>
-</motion.h1>
-{/* subtitle */}
-<motion.p
-  variants={fadeIn("down", 0.3)}
-  initial="hidden"
-  animate="show"
-  exit="hidden"
-  className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
->
-  12+ years helping organizations across ASEAN and APAC uncover
-  hidden opportunities, manage risk, and make informed decisions
-  — transforming complex data into clear, actionable business outcomes.
-</motion.p>
-
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Turning Data <br /> Into{" "}
+            <span className="text-accent">Business Value.</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            12+ years helping organizations across ASEAN and APAC uncover
+            hidden opportunities, manage risk, and make informed decisions
+            — transforming complex data into clear, actionable business outcomes.
+          </motion.p>
           {/* btn */}
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
@@ -60,10 +56,8 @@ const Home = () => {
           className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
           aria-hidden
         />
-
         {/* particles */}
         <ParticlesContainer />
-
         {/* avatar */}
         <motion.div
           variants={fadeIn("up", 0.5)}
