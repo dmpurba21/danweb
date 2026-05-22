0eb2c01acdd69add23e0e323406b8ade181d63f2
dmpurba21 <dmpurba@icloud.com>
2026-05-22 15:51:00 +0700

    index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,30 +13,29 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <motion.h1
-            variants={fadeIn("down", 0.2)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="h1"
-          >
-            Transforming Ideas <br /> Into{" "}
-            <span className="text-accent">Digital Reality</span>
-          </motion.h1>
-
-          {/* subtitle */}
-          <motion.p
-            variants={fadeIn("down", 0.3)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
-          >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
-            exercitationem harum, quia nulla temporibus deleniti libero veniam
-            vero beatae numquam ducimus illum ab similique ipsam tempore fugit
-            quod laudantium debitis.
-          </motion.p>
+         {/* title */}
+<motion.h1
+  variants={fadeIn("down", 0.2)}
+  initial="hidden"
+  animate="show"
+  exit="hidden"
+  className="h1"
+>
+  Turning Data <br /> Into{" "}
+  <span className="text-accent">Business Value.</span>
+</motion.h1>
+{/* subtitle */}
+<motion.p
+  variants={fadeIn("down", 0.3)}
+  initial="hidden"
+  animate="show"
+  exit="hidden"
+  className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+>
+  12+ years helping organizations across ASEAN and APAC uncover
+  hidden opportunities, manage risk, and make informed decisions
+  — transforming complex data into clear, actionable business outcomes.
+</motion.p>
 
           {/* btn */}
           <div className="flex justify-center xl:hidden relative">
