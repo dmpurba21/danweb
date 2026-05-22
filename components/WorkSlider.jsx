365e97d8e1516ce1bb333d67a31ce69df5ac003e
dmpurba21 <dmpurba@icloud.com>
2026-05-22 21:11:37 +0700

    Update WorkSlider.jsx

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -1,11 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
-import { BsArrowRight } from "react-icons/bs";
-import { Pagination } from "swiper";
+import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
-import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
 const workSlides = {
@@ -13,46 +9,20 @@ const workSlides = {
     {
       images: [
         {
-          title: "title",
-          path: "/thumb5.png",
-        },
-        {
-          title: "title",
-          path: "/thumb6.png",
-        },
-        {
-          title: "title",
-          path: "/thumb3.jpg",
-          link: "http://example.com",
-        },
-        {
-          title: "title",
-          path: "/thumb4.jpg",
-          link: "http://example.com",
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: "title",
-          path: "/thumb4.jpg",
-          link: "http://example.com",
-        },
-        {
-          title: "title",
-          path: "/thumb1.jpg",
-          link: "http://example.com",
-        },
-        {
-          title: "title",
+          title: "Vendor Cyber Risk Assessment",
           path: "/thumb2.jpg",
-          link: "http://example.com",
         },
         {
-          title: "title",
+          title: "Financial Standing Analysis",
           path: "/thumb3.jpg",
-          link: "http://example.com",
+        },
+        {
+          title: "Software License Audit",
+          path: "/thumb4.jpg",
+        },
+        {
+          title: "Compliance Dashboard",
+          path: "/thumb5.png",
         },
       ],
     },
@@ -78,39 +48,20 @@ const WorkSlider = () => {
                 key={imageI}
               >
                 <div className="flex items-center justify-center relative overflow-hidden group">
-                  {/* image */}
                   <Image
                     src={image.path}
                     alt={image.title}
                     width={500}
                     height={300}
                   />
-
-                  {/* overlay gradient */}
                   <div
                     className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"
                     aria-hidden
                   />
-
-                  {/* title */}
                   <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                    <Link
-                      href={image.link}
-                      target="_blank"
-                      rel="noreferrer noopener"
-                      className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]"
-                    >
-                      {/* title part 1 */}
-                      <div className="delay-100">LIVE</div>
-                      {/* title part 2 */}
-                      <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                        PROJECT
-                      </div>
-                      {/* icon */}
-                      <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                        <BsArrowRight aria-hidden />
-                      </div>
-                    </Link>
+                    <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                      <div className="delay-100">{image.title}</div>
+                    </div>
                   </div>
                 </div>
               </div>
