935bb024273af7f2416d4cd3a0a47b26abdbf6a8
dmpurba21 <dmpurba@icloud.com>
2026-05-22 15:36:50 +0700

    Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Link from "next/link";
-
 import Socials from "../components/Socials";
 
 const Header = () => {
@@ -10,15 +8,12 @@ const Header = () => {
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
           <Link href="/">
-            <Image
-              src="/logo.svg"
-              alt="logo"
-              width={220}
-              height={48}
-              priority
-            />
+            <div className="text-2xl font-bold">
+              <span className="text-white">daniel </span>
+              <span className="text-white">purba</span>
+              <span className="text-accent">.</span>
+            </div>
           </Link>
-
           {/* socials */}
           <Socials />
         </div>
