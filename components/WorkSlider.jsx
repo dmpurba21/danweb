c6263baf8d2915a973f3808fe0bab7a64985ae21
Daniel Purba <dmpurba@icloud.com>
2026-06-12 13:38:03 +0700

    fix work slider desktop view

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -38,19 +38,19 @@ const WorkSlider = () => {
       }}
       pagination={{ clickable: true }}
       modules={[Pagination]}
-      className="h-[300px] sm:h-[400px]"
+      className="h-[300px] sm:h-[420px]"
     >
       {workSlides.map((item, i) => (
         <SwiperSlide key={i}>
-          <div className="relative rounded-lg overflow-hidden w-full h-full">
+          <div className="relative rounded-lg overflow-hidden w-full h-full bg-[rgba(65,47,123,0.15)]">
             <Image
               src={item.path}
               alt={item.title}
               fill
-              style={{ objectFit: "contain" }}
+              style={{ objectFit: "cover" }}
             />
-            <div className="absolute bottom-0 left-0 right-0 bg-black/60 px-3 py-2">
-              <div className="text-xs tracking-[0.1em] text-white">
+            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent px-4 py-3">
+              <div className="text-xs tracking-[0.1em] text-white font-medium">
                 {item.title}
               </div>
             </div>
